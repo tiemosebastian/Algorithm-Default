24e6cb98f468ff368f997f8c8d5f8250a78bb559
Tiemo Sebastian <tiemolandes@gmail.com>
2015-05-11 13:56:31 -0700

    Remove length 2 words.
    Remove Boggle words scoring 0 points to comply with the requirements

--- BoggleSolver.java
+++ BoggleSolver.java
@@ -12,7 +12,7 @@ public class BoggleSolver {
     public BoggleSolver(String[] dictionary) {
         dict = new TrieNode(26);
         Stopwatch timer = new Stopwatch();
-        for (String w: dictionary){
+        for (String w: dictionary) {
             dict.add(w);
         }
         StdOut.println("Dictionary initiation time: " + timer.elapsedTime());
@@ -28,8 +28,8 @@ public class BoggleSolver {
         M = board.rows();
         N = board.cols(); 
         TrieSET hits = new TrieSET();
-        boolean visited[] = new boolean[M*N];
-        for (int i = 0; i < M*N; i++){
+        boolean[] visited = new boolean[M*N];
+        for (int i = 0; i < M*N; i++) {
             DFS(0, i,
                     board.getLetter(mapy(i), mapx(i)), 
                     hits, 
@@ -44,29 +44,31 @@ public class BoggleSolver {
      * Runs a Depth First Search on the adjacent fields of the BoggleBoard terminating a branch 
      *  if it is no longer in the trie representation of the dictionary (i.e. Node.next()[cti(nxt)]==null).
      */
-    private void DFS(int count, int pos, char nxt, TrieSET hits, BoggleBoard board, boolean[] visited, TrieNode.Node Nod){
-        if (Nod.next()[cti(nxt)] == null) return;
-        TrieNode.Node Node = Nod.next()[cti(nxt)];
+    private void DFS(int count, int pos, char nxt, TrieSET hits, BoggleBoard board, boolean[] visited, TrieNode.Node nod){
+        if (nod.next()[cti(nxt)] == null) return;
+        TrieNode.Node node = nod.next()[cti(nxt)];
         if (nxt == 'Q') {
-            Node = Node.next()[cti('U')];
+            node = node.next()[cti('U')];
         }
-        boolean vis[] = visited.clone();
-        vis[pos]=true;
-        if (Node.isString()) {
-            hits.add(Node.getString());
+        boolean[] vis = visited.clone();
+        vis[pos] = true;
+        if (node.isString()) {
+            if (node.getString().length() > 2) {
+                hits.add(node.getString());
+            }
         }
         Stack<Integer> DFS = new Stack<Integer>();
         adj(mapx(pos), mapy(pos), DFS);
-        while (!DFS.isEmpty()){
+        while (!DFS.isEmpty()) {
             pos = DFS.pop();
             if (vis[pos]) continue;
             DFS(count+1, 
                     pos,
-                    board.getLetter(mapy(pos),mapx(pos)),
+                    board.getLetter(mapy(pos), mapx(pos)),
                     hits,
                     board,
                     vis,
-                    Node);
+                    node);
         }
     }
     /*
@@ -75,7 +77,7 @@ public class BoggleSolver {
      */
     private int adj(int x, int y, Stack<Integer> S) {
         int count = 0;
-        boolean flag=false;
+        boolean flag = false;
         if (x > 0) {
            S.push(map(x-1, y));
            count++;
@@ -108,7 +110,7 @@ public class BoggleSolver {
                  count++;
                  if (!flag) {
                      S.push(map(x, y+1));
-                     count+=2;
+                     count += 2;
                  }
             }
          }
@@ -120,12 +122,12 @@ public class BoggleSolver {
      * @param word
      * @return
      */
-    public int scoreOf(String word){
+    public int scoreOf(String word) {
         int score = word.length();
-        int qus=0;
+        int qus = 0;
         for (int i = 0; i < score; i++) {
             if (word.charAt(i) == 'Q') {
-                if ( word.charAt(i+1) == 'U') {
+                if (word.charAt(i+1) == 'U') {
                     qus++;
                     i++;
                 }
@@ -147,7 +149,7 @@ public class BoggleSolver {
     * @param c
     * @return
     */
-    private static int cti(char c){
+    private static int cti(char c) {
         return (int) c % 26;
     }
     /**
@@ -157,13 +159,13 @@ public class BoggleSolver {
      * @param y
      * @return
      */
-    private int map(int x, int y){
+    private int map(int x, int y) {
         return y * M + x;
     }
-    private int mapx(int x){
+    private int mapx(int x) {
         return x % M;
     }
-    private int mapy(int y){
+    private int mapy(int y) {
         return (y - y % M) / M;
     }
     /**
@@ -171,10 +173,10 @@ public class BoggleSolver {
      * @param vis
      * @param M
      */
-    private void printvis(boolean vis[], int M){
+    private void printvis(boolean vis[], int M) {
         for(int i = 0; i < M; i++){
             for(int j = 0; j < vis.length / M; j++){
-                if(vis[map(j,i)]) StdOut.print(" 1");
+                if(vis[map(j, i)]) StdOut.print(" 1");
                 else StdOut.print(" 0");
             }
             StdOut.print("\n");
@@ -184,7 +186,7 @@ public class BoggleSolver {
      * Unit testing.
      * @param args
      */
-    public static void main(String args[]){
+    public static void main(String[] args) {
         In in = new In("/Users/tiemo/Desktop/Boggle/boggle/dictionary-yawl.txt");
         String[] dictionary = in.readAllStrings();
         BoggleSolver solver = new BoggleSolver(dictionary);
@@ -192,7 +194,7 @@ public class BoggleSolver {
         int score = 0;
         Stopwatch timer = new Stopwatch();
         int N = 10000;
-        for (int i = 0; i < N; i++){
+        for (int i = 0; i < N; i++) {
             solver.getAllValidWords(board);
             //if (i % 50 == 0) StdOut.println(i);
         }
@@ -200,15 +202,14 @@ public class BoggleSolver {
      //   StdOut.printf("Boggle Solver Time at %d iterations: %f", N, timer.elapsedTime());
         BoggleBoard board1 = new BoggleBoard("/Users/tiemo/Desktop/Boggle/boggle/board-points100.txt");
         timer = new Stopwatch();
-        for (int i = 0; i < N; i++){
+        for (int i = 0; i < N; i++) {
             solver.getAllValidWords(board1);
           //  if (i % 50 == 0) StdOut.println(i);
         }
         StdOut.printf("Boggle Solver Time with 100 solutions at %d iterations: %f", N, timer.elapsedTime());
         StdOut.println("");
         StdOut.printf("Boggle Solver Time with 1250 solutions at %d iterations: %f", N, Time);
-        for (String word : solver.getAllValidWords(board))
-        {
+        for (String word : solver.getAllValidWords(board)) {
             StdOut.print("\n" + word);
             score += solver.scoreOf(word);
         }
